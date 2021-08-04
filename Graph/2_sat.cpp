584e7afc0a0fb29e857ce245fb65b1a9702303e2
SA011 <60836699+SA011@users.noreply.github.com>
2021-08-04 12:17:00 +0600

    Update 2_sat.cpp

--- Graph/2_sat.cpp
+++ Graph/2_sat.cpp
@@ -13,28 +13,24 @@ void add(int a, int b){
     adj[1][b].eb(a);
     adj[1][a ^ 1].eb(b ^ 1);
 }
-void dfs(int ind, int r, int d = -1)
-{
+void dfs(int ind, int r, int d = -1){
     vis[ind] = 1;
     if(r) comp[ind] = d;
     for(auto x:adj[r][ind])
         if(vis[x] == 0) dfs(x,r,d);
     if(!r) ts.push_back(ind);
 }
-bool solve2sat(int n)
-{
+bool solve2sat(int n){
     for(int i = 0; i < n; i++)if(vis[i] == 0) dfs(i, 0);
     for(int i = 0; i < n; i++)vis[i] = 0;
     int com = 0;
-    while(!ts.empty())
-    {
+    while(!ts.empty()){
         if(vis[ts.back()] == 0)
             dfs(ts.back(), 1, com++);
         ts.pop_back();
     }
     
-    for(int i = 0; i < n; i += 2)
-    {
+    for(int i = 0; i < n; i += 2){
         if(comp[i] == comp[i|1])return false;
         sat[i >> 1] = comp[i] > comp[i|1];
     }
