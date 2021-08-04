d4f144af76ebeaf524d241de230cf4af9b0edc2f
SA011 <60836699+SA011@users.noreply.github.com>
2021-08-04 13:26:31 +0600

    Update BIT.cpp

--- DS/BIT.cpp
+++ DS/BIT.cpp
@@ -1,16 +1,15 @@
 #define lastone(n) ((n)&(-(n)))
-ll bit[asz];
-void update(ll ind,ll val)
-{
-    for(++ind;ind<asz;ind+=lastone(ind))bit[ind]+=val;
+ll bit[N];
+void update(int ind, ll val){
+    for(++ind; ind < N; ind += lastone(ind)) 
+        bit[ind] += val;
 }
-ll query(ll a)
-{
-    ll s=0;
-    for(++a;a>0;a-=lastone(a))s+=bit[a];
+ll query(int a){
+    ll s = 0;
+    for(++a; a > 0; a -= lastone(a))
+        s += bit[a];
     return s;
 }
-ll query(ll a,ll b)
-{
-    return query(b)-query(a-1);
+ll query(int a, int b){
+    return query(b) - query(a - 1);
 }
