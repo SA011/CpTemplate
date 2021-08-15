f3ede48cb76a45546a89d7173739f6d8dcc42bb8
SA011 <sk.sabit.287@gmail.com>
2021-08-15 14:21:29 +0600

    update DSU.cpp

--- DS/DSU.cpp
+++ DS/DSU.cpp
@@ -1,17 +1,18 @@
-class Unionfind
-{
+class Unionfind{
 public:
-    vi p,rnk;
-    Unionfind(int N){p.assign(N,0);rnk.assign(N,1);for(int i=0;i<N;i++)p[i]=i;}
-    int findset(int i){return p[i]=(p[i]==i)?i:findset(p[i]);}
-    bool sameset(int i,int j){return findset(i)==findset(j);}
-    void unionset(int i,int j)
-    {
-        int x=findset(i),y=findset(j);
-        if(x!=y)
-        {
-            if(rnk[x]>rnk[y])p[y]=x;
-            else p[x]=y,rnk[y]+=(rnk[x]==rnk[y]);
+    vector<int> p, rnk;
+    int n;
+    Unionfind(int _n) : n(_n) { 
+        p.assign(n, 0), rnk.assign(n, 1); 
+        for(int i = 0; i < n; i++)p[i] = i;
+    }
+    int findset(int i){return p[i] = (p[i] == i) ? i : findset(p[i]);}
+    bool sameset(int i,int j) { return findset(i) == findset(j);}
+    void unionset(int i,int j) {
+        int x=findset(i), y=findset(j);
+        if(x != y){
+            if(rnk[x] > rnk[y]) p[y] = x;
+            else p[x] = y, rnk[y] += (rnk[x] == rnk[y]);
         }
     }
 };
@@ -20,90 +21,81 @@ public:
 //with rollback
 
 struct dsu_save {
-    int v, rnkv, u, rnku,united;
-
+    int v, rnkv, u, rnku, united;
     dsu_save() {}
-
-    dsu_save(int _v, int _rnkv, int _u, int _rnku,int united)
-        : v(_v), rnkv(_rnkv), u(_u), rnku(_rnku),united(united) {}
+    dsu_save(int _v, int _rnkv, int _u, int _rnku, int united)
+        : v(_v), rnkv(_rnkv), u(_u), rnku(_rnku), united(united) {}
 };
-class Unionfind
-{
+class Unionfind{
 public:
-    vi p,rnk;
-    int ans;
+    vector<int> p, rnk;
+    int ans, n;   //CC
     vector<dsu_save> lastadded;
-    Unionfind(int N){ans=N;p.assign(N,0);rnk.assign(N,1);for(int i=0;i<N;i++)p[i]=i;}
-    int findset(int i){return (p[i]==i)?i:findset(p[i]);}
-    bool sameset(int i,int j){return findset(i)==findset(j);}
-    void unionset(int i,int j)
-    {
-        int x=findset(i),y=findset(j);
-        if(x!=y)
-        {
-            ans--;
-            if(rnk[x]>rnk[y])lastadded.emplace_back(y,rnk[y],x,rnk[x],1),p[y]=x;
-            else {lastadded.emplace_back(x,rnk[x],y,rnk[y],1);p[x]=y;if(rnk[x]==rnk[y])rnk[y]++;}
-        }
-        else lastadded.emplace_back(x,rnk[x],y,rnk[y],0);
+    Unionfind(int _n) : n(_n) { 
+        ans = n, p.assign(n, 0), rnk.assign(n, 1);
+        for(int i = 0; i < n; i++) p[i] = i;
     }
-    void rollback()
-    {
+    int findset(int i){return (p[i] == i) ? i : findset(p[i]);}
+    bool sameset(int i,int j){return findset(i) == findset(j);}
+    void unionset(int i,int j){
+        int x=findset(i), y=findset(j);
+        if(x!=y){
+            ans--;
+            if(rnk[x] > rnk[y]) lastadded.emplace_back(y, rnk[y], x, rnk[x], 1), p[y] = x;
+            else lastadded.emplace_back(x, rnk[x], y, rnk[y], 1), p[x] = y, rnk[y] += (rnk[x] == rnk[y]);
+        }
+        else lastadded.emplace_back(x, rnk[x], y, rnk[y], 0);
+    }
+    void rollback(){
         if(lastadded.empty())return;
-        auto x=lastadded.back();
+        auto x = lastadded.back();
         lastadded.pop_back();
-        if(x.united==0)return;
+        if(!x.united)return;
         ans++;
-        p[x.v]=x.v;p[x.u]=x.u;
-        rnk[x.v]=x.rnkv;
-        rnk[x.u]=x.rnku;
+        p[x.v] = x.v, p[x.u] = x.u;
+        rnk[x.v] = x.rnkv, rnk[x.u] = x.rnku;
     }
 };
 
 
 //online bipartite check with rollback
-
 struct dsu_save {
-    int v, rnkv, u, rnku,united,valv,valu;
-
+    int v, rnkv, u, rnku, united, valv, valu;
     dsu_save() {}
-
-    dsu_save(int _v, int _rnkv, int _u, int _rnku,int united,int _vv,int _vu)
-        : v(_v), rnkv(_rnkv), u(_u), rnku(_rnku),united(united),valv(_vv),valu(_vu) {}
+    dsu_save(int _v, int _rnkv, int _u, int _rnku, int united, int _vv, int _vu)
+        : v(_v), rnkv(_rnkv), u(_u), rnku(_rnku), united(united), valv(_vv), valu(_vu) {}
 };
-class Unionfind
-{
+class Unionfind{
 public:
-    vi p,rnk,val;
+    vector<int> p, rnk, val;
     vector<dsu_save> lastadded;
-    Unionfind(int N){val.assign(N,0);p.assign(N,0);rnk.assign(N,1);for(int i=0;i<N;i++)p[i]=i;}
-    int findset(int i,int &t){
-        return t+=val[i],(p[i]==i)?i:findset(p[i],t);
+    int n;
+    Unionfind(int _n) : n(_n) {
+        val.assign(n, 0), p.assign(n,0), rnk.assign(n, 1);
+        for(int i = 0; i < n; i++) p[i] = i;
     }
-    // bool sameset(int i,int j){return findset(i)==findset(j);}
-    bool unionset(int i,int j)
-    {
-        int a=0,b=0;
-        int x=findset(i,a);
-        int y=findset(j,b);
-        if(x!=y)
-        {
-            if(rnk[x]>rnk[y])lastadded.emplace_back(y,rnk[y],x,rnk[x],1,val[y],val[x]),p[y]=x,val[y]=1+(a%2!=b%2);
-            else {lastadded.emplace_back(x,rnk[x],y,rnk[y],1,val[x],val[y]);p[x]=y;if(rnk[x]==rnk[y])rnk[y]++;val[x]=1+(a%2!=b%2);}
+    int findset(int i,int &t){
+        return t += val[i], (p[i] == i) ? i : findset(p[i], t);
+    }
+    bool unionset(int i,int j){
+        int a = 0, b = 0;
+        int x = findset(i,a), y = findset(j,b);
+        if(x != y){
+            if(rnk[x] > rnk[y])
+                lastadded.emplace_back(y, rnk[y], x, rnk[x], 1, val[y], val[x]), p[y] = x, val[y] = 1 + ((a ^ b) & 1);
+            else 
+                lastadded.emplace_back(x, rnk[x], y, rnk[y], 1, val[x], val[y]), p[x] = y, rnk[y] += (rnk[x]==rnk[y]), val[x] = 1 + ((a ^ b) & 1);}
         }
-        else return lastadded.emplace_back(x,rnk[x],y,rnk[y],0,val[x],val[y]),a%2!=b%2;
+        else return lastadded.emplace_back(x, rnk[x], y, rnk[y], 0, val[x], val[y]), (a ^ b) & 1;
         return true;
     }
-    void rollback()
-    {
+    void rollback(){
         if(lastadded.empty())return;
-        auto x=lastadded.back();
+        auto x = lastadded.back();
         lastadded.pop_back();
-        if(x.united==0)return;
-        p[x.v]=x.v;p[x.u]=x.u;
-        rnk[x.v]=x.rnkv;
-        rnk[x.u]=x.rnku;
-        val[x.v]=x.valv;
-        val[x.u]=x.valu;
+        if(!x.united)return;
+        p[x.v] = x.v, p[x.u] = x.u;
+        rnk[x.v] = x.rnkv, rnk[x.u] = x.rnku;
+        val[x.v] = x.valv, val[x.u] = x.valu;
     }
 };
