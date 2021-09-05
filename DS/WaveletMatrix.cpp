f81d624f8a4ba31895770d6f0af623bf39d262a9
SA011 <sk.sabit.287@gmail.com>
2021-09-05 10:50:02 +0600

    update Treap

--- DS/WaveletMatrix.cpp
+++ DS/WaveletMatrix.cpp
@@ -1,13 +1,12 @@
-
 struct BitVector{
 //pragma GCC target ("sse4.2")
     using T = unsigned int;
     int n, blk;
-    vector<T> bits,cnt;
-    BitVector(int _n = 0):n(_n){
-        blk = (n + 32) >> 5;
-        bits.assign(blk,0);
-        cnt.assign(blk,0);
+    vector<T> bits, cnt;
+    BitVector(int _n = 0) : n(_n){
+        blk = (n >> 5) + 3;
+        bits.assign(blk, 0);
+        cnt.assign(blk, 0);
     }
     inline bool operator[] (int i){ return (bits[i >> 5] >> (i & 31)) & 1;}
     inline void set(int i){ bits[i >> 5] |= 1 << (i & 31);}
@@ -18,11 +17,11 @@ struct BitVector{
             cur += __builtin_popcountll(bits[i]);
         }
     }
-    inline T count(int l) { return cnt[l>>5] + __builtin_popcountll(bits[l>>5] & ((1 << (l&31))-1));}
-    inline T count(int dig,int l) {return dig ? count(l) : l - count(l);}
-    inline T count(int dig,int l,int r) { return count(dig,r) - count(dig,l);}
+    inline T count(int l) { return cnt[l >> 5] + __builtin_popcountll(bits[l >> 5] & ((1 << (l & 31)) - 1));}
+    inline T count(int dig, int l) {return dig ? count(l) : l - count(l);}
+    inline T count(int dig, int l, int r) { return count(dig, r) - count(dig, l);}
     inline bool swap(int l){
-        if(operator [](l) == operator [](l+1))return false;
+        if(operator [](l) == operator [](l + 1))return false;
         if((l & 31) == 31){
             if(operator [](l))cnt[l >> 5]--, cnt[(l >> 5) + 1]++, reset(l), set(l + 1);
             else cnt[l >> 5]++, cnt[(l >> 5) + 1]--, set(l), reset(l + 1);
@@ -33,6 +32,7 @@ struct BitVector{
         return true;
     }
 };
+
 struct WaveletMatrix{
     using T = ll;
     vector<BitVector> lvl;
@@ -42,13 +42,13 @@ struct WaveletMatrix{
     void build(vector<T> v){
         v.push_back(0);
         n = v.size();
-        offset = -*min_element(v.begin(),v.end());
+        offset = -*min_element(v.begin(), v.end());
         for(auto &x:v)x += offset;
-        h = *max_element(v.begin(),v.end());
+        h = *max_element(v.begin(), v.end());
         h = 64 - __builtin_clzll(h);
         lvl.assign(h, BitVector(n));
-        mid.assign(h,0);
-        T msk = 1LL << (h-1);
+        mid.assign(h, 0);
+        T msk = 1LL << (h - 1);
         vector<T> temp = v;
         
         for(int i = 0; i < h; i++){
@@ -56,7 +56,7 @@ struct WaveletMatrix{
             for(auto &x:v)z += !(x & msk);
             mid[i] = z;
             for(int j = 0, k = 0; j < n; j++){
-                if(v[j] & msk)temp[z++] = v[j], lvl[i].set(j);
+                if(v[j] & msk) temp[z++] = v[j], lvl[i].set(j);
                 else temp[k++] = v[j], lvl[i].reset(j);
             }
             v = temp;
@@ -77,7 +77,7 @@ struct WaveletMatrix{
         for(int i = 0; i < h; i++){
             int cur = lvl[i][ind];
             res = (res << 1) | cur;
-            ind = lvl[i].count(cur,ind);
+            ind = lvl[i].count(cur, ind);
             if(cur) ind += mid[i];
         }
         return res - offset;
@@ -85,23 +85,25 @@ struct WaveletMatrix{
     
     T quantile(int l, int r, int k, bool rev = false){
         //k th smallest ( largest if rev = true ) element in [l,r)
-        if(l < 0 || r >= n || k < 0 || k >= r-l)return -1;
+        if(l < 0 || r >= n || k < 0 || k >= r - l)return -1;
         T res = 0;
         for(int i = 0; i < h; i++){
             int temp = lvl[i].count(rev, l, r);
             int cur = (k >= temp) ^ rev;
             res = (res << 1) | cur;
-            if(cur ^ rev)k -= temp;
-            l = lvl[i].count(cur,l), r = lvl[i].count(cur,r);
+            if(cur ^ rev) k -= temp;
+            l = lvl[i].count(cur, l), r = lvl[i].count(cur, r);
             if(cur) l += mid[i], r += mid[i];
         }
         return res - offset;
     }
-    tuple<int,int,int> rank(int l, int r, T v){
+    tuple<int, int, int> rank(int l, int r, T v){
         int ls = 0, gt = 0;
         v += offset;
+        if(v < 0)return make_tuple(0, 0, r - l);
+        
         for(int i = 0; i < h; i++){
-            int cur = (v >> (h-1-i)) & 1;
+            int cur = (v >> (h - 1 - i)) & 1;
             int nl = lvl[i].count(cur, l), nr = lvl[i].count(cur, r);
             if(cur)ls += (r - l) - (nr - nl);
             else gt += (r - l) - (nr - nl);
@@ -109,14 +111,14 @@ struct WaveletMatrix{
             l = nl, r = nr;
             if(cur)l += mid[i], r += mid[i];
         }
-        return make_tuple(ls,r-l,gt);
+        return make_tuple(ls, r - l, gt);
     }
     void swap(int ind){
         if(ind < 0 || ind + 1 >= n)return;
         for(int i = 0; i < h; i++){
             if(lvl[i].swap(ind))break;
             bool cur = lvl[i][ind];
-            ind = lvl[i].count(cur,ind);
+            ind = lvl[i].count(cur, ind);
             if(cur)ind += mid[i];
         }
     }
