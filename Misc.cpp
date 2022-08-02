24e9764d4f4dc2ce002e7e784db2c0a045f1e98d
SA011 <60836699+SA011@users.noreply.github.com>
2022-08-02 20:48:28 +0600

    Update Misc.cpp

--- Misc.cpp
+++ Misc.cpp
@@ -4,3 +4,8 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 //inverse (Linear)
 inv[1]=1;
 for(int i=2;i<asz;i++)inv[i]=(mod-(mod/i)*inv[mod%i]%mod)%mod;
+
+
+//Grundy Cycle
+Grundy[i] = mex(Grundy[j] ^ Grundy[i - j - 2]) 
+There is a cycle of length 34 after first few value.
